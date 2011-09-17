4199d9af6c8a1a5741b07a535f7d65fc0a5602d6
hirokuma <uenokuma@gmail.com>
2011-09-17 19:13:27 +0900

    サンプル追加

---
+++ sample/src/com/nfcpcdsample/MainActivity.java
@@ -0,0 +1,77 @@
+package com.nfcpcdsample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.blogpost.hiro99ma.pcd.NfcF;
+import com.blogpost.hiro99ma.pcd.NfcPcd;
+
+public class MainActivity extends Activity implements com.blogpost.hiro99ma.pcd.UsbHost.UsbListener {
+	private TextView mResultText = null;
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    	com.blogpost.hiro99ma.pcd.UsbHost usbHost = new com.blogpost.hiro99ma.pcd.UsbHost();
+        boolean ret = usbHost.onCreate(this);
+        if(ret) {
+            Button btn = (Button)findViewById(R.id.button_read);
+            btn.setOnClickListener(mReadListener);
+            mResultText = (TextView)findViewById(R.id.text_result);
+        }
+    }
+
+	@Override
+	public void inserted() {
+		Toast.makeText(this, "inserted", Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public void removed() {
+		Toast.makeText(this, "removed", Toast.LENGTH_LONG).show();
+		finish();
+	}
+
+    private OnClickListener mReadListener = new OnClickListener() {
+        public void onClick(View v) {
+    		final NfcPcd.NfcId nfcid = NfcPcd.getNfcId();
+
+        	StringBuffer sb = null;
+        	String title = "polling";
+        	String sub = "";
+    		boolean ret = NfcPcd.pollingF(NfcF.SYSCODE);
+        	boolean ret2 = false;
+        	if(!ret) {
+        		ret2 = NfcPcd.pollingF();
+        	}
+        	if(ret || ret2) {
+        		ret = true;
+    			sub = "(System Code:" + String.format("%04x", nfcid.SensRes & 0xffff) + ")";
+        	} else {
+        		ret = NfcPcd.pollingA();
+        	}
+        	if(ret) {
+        		//NfcIdは1つしかないので、ちょっと格好が悪いな
+        		title = nfcid.Label;
+    			byte len = nfcid.Length;
+        		sb = new StringBuffer(len * 2);
+        		sb.append(String.format("%02x", nfcid.Id[0] & 0xff));
+    			for(byte i=1; i<len; i++) {
+    				sb.append("-" + String.format("%02x", nfcid.Id[i] & 0xff));
+    			}
+        	} else {
+            	sb = new StringBuffer("fail.");
+        	}
+        	mResultText.setText(title + " : "+ sb + sub);
+        	NfcPcd.rfOff();
+        }
+    };
+
+}
