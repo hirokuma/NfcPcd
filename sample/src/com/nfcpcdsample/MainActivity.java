78cab6111a5ec23ef8a9d4a11df2b4135710dfff
hirokuma <uenokuma@gmail.com>
2011-09-29 00:51:32 +0900

    add onDestroy()

--- sample/src/com/nfcpcdsample/MainActivity.java
+++ sample/src/com/nfcpcdsample/MainActivity.java
@@ -13,20 +13,27 @@ import com.blogpost.hiro99ma.pcd.NfcPcd;
 
 public class MainActivity extends Activity implements com.blogpost.hiro99ma.pcd.UsbHost.UsbListener {
 	private TextView mResultText = null;
+	private com.blogpost.hiro99ma.pcd.UsbHost mUsbHost = null;
 
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
-    	com.blogpost.hiro99ma.pcd.UsbHost usbHost = new com.blogpost.hiro99ma.pcd.UsbHost();
-        boolean ret = usbHost.onCreate(this);
-        if(ret) {
-            Button btn = (Button)findViewById(R.id.button_read);
-            btn.setOnClickListener(mReadListener);
-            mResultText = (TextView)findViewById(R.id.text_result);
-        }
-    }
+		mUsbHost = new com.blogpost.hiro99ma.pcd.UsbHost();
+		boolean ret = mUsbHost.onCreate(this);
+		if(ret) {
+			Button btn = (Button)findViewById(R.id.button_read);
+			btn.setOnClickListener(mReadListener);
+			mResultText = (TextView)findViewById(R.id.text_result);
+		}
+	}
+
+	@Override
+	public void onDestroy() {
+		mUsbHost.onDestroy(this);
+		super.onDestroy();
+	}
 
 	@Override
 	public void inserted() {
@@ -39,39 +46,39 @@ public class MainActivity extends Activity implements com.blogpost.hiro99ma.pcd.
 		finish();
 	}
 
-    private OnClickListener mReadListener = new OnClickListener() {
-        public void onClick(View v) {
-    		final NfcPcd.NfcId nfcid = NfcPcd.getNfcId();
+	private OnClickListener mReadListener = new OnClickListener() {
+		public void onClick(View v) {
+			final NfcPcd.NfcId nfcid = NfcPcd.getNfcId();
 
-        	StringBuffer sb = null;
-        	String title = "polling";
-        	String sub = "";
-    		boolean ret = NfcPcd.pollingF(NfcF.SYSCODE);
-        	boolean ret2 = false;
-        	if(!ret) {
-        		ret2 = NfcPcd.pollingF();
-        	}
-        	if(ret || ret2) {
-        		ret = true;
-    			sub = "(System Code:" + String.format("%04x", nfcid.SensRes & 0xffff) + ")";
-        	} else {
-        		ret = NfcPcd.pollingA();
-        	}
-        	if(ret) {
-        		//NfcIdは1つしかないので、ちょっと格好が悪いな
-        		title = nfcid.Label;
-    			byte len = nfcid.Length;
-        		sb = new StringBuffer(len * 2);
-        		sb.append(String.format("%02x", nfcid.Id[0] & 0xff));
-    			for(byte i=1; i<len; i++) {
-    				sb.append("-" + String.format("%02x", nfcid.Id[i] & 0xff));
-    			}
-        	} else {
-            	sb = new StringBuffer("fail.");
-        	}
-        	mResultText.setText(title + " : "+ sb + sub);
-        	NfcPcd.rfOff();
-        }
-    };
+			StringBuffer sb = null;
+			String title = "polling";
+			String sub = "";
+			boolean ret = NfcPcd.pollingF(NfcF.SYSCODE);
+			boolean ret2 = false;
+			if(!ret) {
+				ret2 = NfcPcd.pollingF();
+			}
+			if(ret || ret2) {
+				ret = true;
+				sub = "(System Code:" + String.format("%04x", nfcid.SensRes & 0xffff) + ")";
+			} else {
+				ret = NfcPcd.pollingA();
+			}
+			if(ret) {
+				//NfcIdは1つしかないので、ちょっと格好が悪いな
+				title = nfcid.Label;
+				byte len = nfcid.Length;
+				sb = new StringBuffer(len * 2);
+				sb.append(String.format("%02x", nfcid.Id[0] & 0xff));
+				for(byte i=1; i<len; i++) {
+					sb.append("-" + String.format("%02x", nfcid.Id[i] & 0xff));
+				}
+			} else {
+				sb = new StringBuffer("fail.");
+			}
+			mResultText.setText(title + " : "+ sb + sub);
+			NfcPcd.rfOff();
+		}
+	};
 
 }
